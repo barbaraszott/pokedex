e4e20e2dc583b45497cab1846b5b185e6737bb23
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-06 18:46:05 +0200

    Fetch data from API on componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from "react";
-import "./App.css";
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return <h1>Henlo</h1>;
+class App extends Component {
+  state = {
+    pokemons: [],
+  };
+
+  catchPokemonData = (pokemon) => {
+    return axios.get(pokemon.url).then((response) => response.data);
+  };
+
+  gottaCatchThemAll = async () => {
+    const pokemonList = await axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) => response.data.results);
+    const promises = pokemonList.map(this.catchPokemonData);
+    const pokemons = await Promise.all(promises);
+
+    this.setState({
+      pokemons,
+    });
+  };
+
+  componentDidMount() {
+    this.gottaCatchThemAll();
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
 }
 
 export default App;
