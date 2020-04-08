5ac71352d2fda0d2af3f708293e23f62bffa6daf
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-08 12:00:23 +0200

    Move function fetching pokemons' list to api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+export async function gottaCatchThemAll() {
+  const pokemonList = await axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) => response.data.results);
+  const promises = pokemonList.map((pokemon) => catchPokemonData(pokemon.name));
+  const pokemons = await Promise.all(promises);
+
+  return pokemons;
+}
+
 export function catchPokemonData(name) {
   return axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`).then((response) => response.data);
 }
--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,17 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import axios from "axios";
 
 import PokemonListItem from "./PokemonListItem";
-import { catchPokemonData } from "../api";
-
-async function gottaCatchThemAll() {
-  const pokemonList = await axios.get("https://pokeapi.co/api/v2/pokemon/").then((response) => response.data.results);
-  const promises = pokemonList.map((pokemon) => catchPokemonData(pokemon.name));
-  const pokemons = await Promise.all(promises);
-
-  return pokemons;
-}
+import { gottaCatchThemAll } from "../api";
 
 function PokemonList() {
   const [pokemonList, setPokemonList] = useState([]);
