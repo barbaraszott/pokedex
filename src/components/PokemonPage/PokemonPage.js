3cb7e4c70254120ba7b55c2f521dbead7c2533e1
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-10 23:11:23 +0200

    Update PokemonPage - get color so it can be used as background-color, change text of going back to list to an left-arrow

--- src/components/PokemonPage/PokemonPage.js
+++ src/components/PokemonPage/PokemonPage.js
@@ -1,26 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
-import Pokemon from "./Pokemon";
-import { catchPokemonData } from "../api";
+import getPokemonColor from "../../tools/pokemonColor";
+import Pokemon from "../Pokemon/Pokemon";
+import { catchPokemonData } from "../../api/api";
+
+import "./PokemonPage.scss";
 
 function PokemonPage() {
   const [pokemonData, setPokemonData] = useState(null);
-
+  const [pokemonColor, setPokemonColor] = useState(null);
   const { name } = useParams();
 
   useEffect(() => {
     catchPokemonData(name).then((data) => {
       setPokemonData(data);
       document.title = `Viewing ${data.name}`;
+      setPokemonColor(getPokemonColor(data.color));
     });
   }, [name]);
 
   return (
-    <>
-      <Link to="/">Go Back to Pokemon List</Link>
+    <section className="pokemon-page" style={{ backgroundColor: pokemonColor }}>
+      <Link to="/" className="back-arrow">
+        &#129104;
+      </Link>
       {pokemonData ? <Pokemon {...pokemonData} /> : "Loading"}
-    </>
+    </section>
   );
 }
 
