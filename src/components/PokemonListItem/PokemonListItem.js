747650a2aa58707b411dd14041e005283984c56f
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-10 23:08:41 +0200

    Update PokemonListItem - set background color, picture

---
+++ src/components/PokemonListItem/PokemonListItem.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import getPokemonColor from "../../tools/pokemonColor";
+import "./PokemonListItem.scss";
+import * as noPic from "../../tools/no-picture.png";
+
+function PokemonListItem(props) {
+  const { name, id, sprites, types, color } = props;
+
+  const backgroundColor = {
+    backgroundColor: getPokemonColor(color),
+  };
+
+  const pokemonPicture = sprites.front_default ? sprites.front_default : noPic;
+
+  return (
+    <div className="list-item-container" style={backgroundColor}>
+      <h2 className="name">
+        {name} <span className="id">#{id}</span>
+      </h2>
+      <section className="info">
+        <figure className="pokemon-img-container">
+          <img src={pokemonPicture} alt="" className="pokemon-img-container__img"></img>
+        </figure>
+        <div className="types-container">
+          {types.map((type) => (
+            <span className="types-container__type" key={`${name}-${type}`}>
+              {type}
+            </span>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default PokemonListItem;
