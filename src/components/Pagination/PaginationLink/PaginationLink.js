84e7800969d137dd358abe3b99d572b11e519343
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-12 14:26:40 +0200

    Remove font-awesome

--- src/components/Pagination/PaginationLink/PaginationLink.js
+++ src/components/Pagination/PaginationLink/PaginationLink.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
 import classNames from "classnames";
 
 import "./PaginationLink.scss";
@@ -9,11 +7,7 @@ function PaginationLink(props) {
   const { pageIndex, currentPageIndex, onPageClick, isLinkToStart, isLinkToEnd, isPrevious, isNext } = props;
   const isCurrentPage = pageIndex === currentPageIndex;
   const pageNumber = pageIndex + 1;
-  const createArrow = (isNext, isPrevious) => {
-    if (isPrevious) return <FontAwesomeIcon icon={faChevronLeft} />;
-    if (isNext) return <FontAwesomeIcon icon={faChevronRight} />;
-  };
-  const content = isNext || isPrevious ? createArrow(isNext, isPrevious) : pageNumber;
+  const content = isPrevious || isNext ? "" : pageNumber;
 
   const classes = classNames("pagination__link", {
     "pagination__link--current": isCurrentPage,
@@ -24,13 +18,9 @@ function PaginationLink(props) {
   });
 
   return (
-    <>
-      {isLinkToEnd && <FontAwesomeIcon icon={faEllipsisH} className="dots" />}
-      <div data-page={pageIndex} className={classes} onClick={() => onPageClick(pageIndex)}>
-        {content}
-      </div>
-      {isLinkToStart && <FontAwesomeIcon icon={faEllipsisH} className="dots" />}
-    </>
+    <div data-page={pageIndex} className={classes} onClick={() => onPageClick(pageIndex)}>
+      {content}
+    </div>
   );
 }
 
