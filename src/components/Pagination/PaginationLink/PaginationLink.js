a41232859d3c727f9a1580ae7bf98a22b0b2f579
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-12 13:30:18 +0200

    Create class names with the use of classnames library

--- src/components/Pagination/PaginationLink/PaginationLink.js
+++ src/components/Pagination/PaginationLink/PaginationLink.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft, faChevronRight, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import classNames from "classnames";
+
+import "./PaginationLink.scss";
 
 function PaginationLink(props) {
-  const { page, currentPage, onPageClick, isLinkToStart, isLinkToEnd, isPrevious, isNext } = props;
-  const isCurrentPage = page === currentPage;
+  const { pageIndex, currentPageIndex, onPageClick, isLinkToStart, isLinkToEnd, isPrevious, isNext } = props;
+  const isCurrentPage = pageIndex === currentPageIndex;
+  const pageNumber = pageIndex + 1;
+
+  const classes = classNames("pagination__link", {
+    "pagination__link--current": isCurrentPage,
+    "pagination__link--start": isLinkToStart,
+    "pagination__link--end": isLinkToEnd,
+    "pagination__link--previous": isPrevious,
+    "pagination__link--next": isNext,
+  });
+
 }
 
 export default PaginationLink;
