6a01fc90e3c0db590dccc03bcfcfd4336a6a647e
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-12 11:27:26 +0200

    Refactor upper side (header part) of single pokemon page

--- src/components/Pokemon/Pokemon.js
+++ src/components/Pokemon/Pokemon.js
@@ -10,7 +10,7 @@ import * as noPic from "../../tools/no-picture.png";
 import "./Pokemon.scss";
 
 function Pokemon(props) {
-  const { name, id, height, sprites, stats, weight, abilities, types } = props;
+  const { name, id, height, sprites, stats, weight, abilities, types, backgroundColor } = props;
 
   const pokemonPicture = sprites.front_default ? sprites.front_default : noPic;
 
@@ -18,19 +18,21 @@ function Pokemon(props) {
 
   return (
     <>
-      <h1 className="pokemon-page__name">
-        {name} <span className="pokemon-page__id">#{id}</span>
-      </h1>
-      <div className="pokemon-page__types-container">
-        {types.map((type) => (
-          <span className="pokemon-page__types-container__type" key={`${name}-${type}`}>
-            {type}
-          </span>
-        ))}
-      </div>
-      <figure className="pokemon-page__img-container">
-        <img src={pokemonPicture} alt="" className="pokemon-page__img-container__img"></img>
-      </figure>
+      <section className="pokemon__header" style={{ backgroundColor }}>
+        <h1 className="pokemon-page__name">
+          {name} <span className="pokemon-page__id">#{id}</span>
+        </h1>
+        <div className="pokemon-page__types-container">
+          {types.map((type) => (
+            <span className="pokemon-page__types-container__type" key={`${name}-${type}`}>
+              {type}
+            </span>
+          ))}
+        </div>
+        <figure className="pokemon-page__img-container">
+          <img src={pokemonPicture} alt="" className="pokemon-page__img-container__img"></img>
+        </figure>
+      </section>
       <main className="pokemon-info__content">
         <nav className="pokemon-info__navigation">
           <NavLink to={`${routeMatch.url}`}>About</NavLink>
