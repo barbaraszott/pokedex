bdf0ed8801d2ed4b687f3b68e31880cfb6aa8916
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-13 12:16:30 +0200

    Add aria-label for next/previous buttons in pagination

--- src/components/Pagination/PaginationLink/PaginationLink.js
+++ src/components/Pagination/PaginationLink/PaginationLink.js
@@ -8,6 +8,7 @@ function PaginationLink(props) {
   const isCurrentPage = pageIndex === currentPageIndex;
   const pageNumber = pageIndex + 1;
   const content = isPrevious || isNext ? "" : pageNumber;
+  const ariaLabel = isPrevious ? "Previous page" : isNext ? "Next page" : "";
 
   const classes = classNames("pagination__link", {
     "pagination__link--current": isCurrentPage,
@@ -18,7 +19,7 @@ function PaginationLink(props) {
   });
 
   return (
-    <button data-page={pageIndex} className={classes} role="button" onClick={() => onPageClick(pageIndex)}>
+    <button data-page={pageIndex} className={classes} aria-label={ariaLabel} onClick={() => onPageClick(pageIndex)}>
       {content}
     </button>
   );
