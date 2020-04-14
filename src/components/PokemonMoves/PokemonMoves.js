d69abdb99f7cd69bb4ff60a1618431daee6843e6
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-14 14:50:02 +0200

    Create content for PokemonMoves - list with moves

--- src/components/PokemonMoves/PokemonMoves.js
+++ src/components/PokemonMoves/PokemonMoves.js
@@ -1,7 +1,23 @@
 import React from "react";
 
+import "./PokemonMoves.scss";
+
 function PokemonMoves(props) {
-  return <h2>Moves</h2>;
+  const name = props.name[0].toUpperCase() + props.name.substr(1);
+  const moves = props.moves.sort();
+
+  return (
+    <>
+      <h2>All possible {name}'s moves</h2>
+      <ul className="pokemon-moves-list">
+        {moves.map((move) => (
+          <li className="pokemon-moves-list__item" key={`pokemon-moves-${move}`}>
+            {move.replace("-", " ")}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default PokemonMoves;
