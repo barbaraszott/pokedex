b895f1694adca5bcafb007da8ebea5f0d807f88d
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-14 17:58:01 +0200

    Fetch evolution data when PokemonEvolution component is mounted
    Show Spinner while fetching

--- src/components/PokemonEvolution/PokemonEvolution.js
+++ src/components/PokemonEvolution/PokemonEvolution.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import "./PokemonEvolution.scss";
+import Spinner from "../../tools/Spinner";
 
 function PokemonEvolution(props) {
-  return <h2>Evolution</h2>;
+  const { evolutionChainUrl, getEvolutionChain } = props;
+
+  const [isLoading, setLoading] = useState(true);
+  const [evolutions, setEvolutions] = useState({});
+
+  useEffect(() => {
+    getEvolutionChain(evolutionChainUrl).then((evolutions) => {
+      setEvolutions(evolutions);
+      setLoading(false);
+    });
+  }, [evolutionChainUrl, getEvolutionChain]);
+
+  return <>{isLoading ? <Spinner /> : <span>Evolutions fetched</span>}</>;
 }
 
-export default PokemonEvolution;
+export default React.memo(PokemonEvolution);
