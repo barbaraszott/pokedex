3242d61e35bff3ef847c54dc80435cb96ec28566
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-15 13:34:46 +0200

    Update tests for PokemonList

--- src/components/PokemonList/PokemonList.test.js
+++ src/components/PokemonList/PokemonList.test.js
@@ -1,16 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { render, waitForElementToBeRemoved } from "@testing-library/react";
 import { waitFor } from "@testing-library/dom";
 import PokemonList from "./PokemonList";
-import ReactDOM from "react-dom";
-import { act } from "react-dom/test-utils";
 
 import pokemonsData from "../../api/pokemons.json";
 
 it("should display spinner while fetching data", async () => {
   const { getByTestId, queryByTestId } = render(
-    <PokemonList gottaCatchThemAll={() => Promise.resolve({ pokemons: [], count: 0 })} />
+    <Router>
+      <PokemonList
+        gottaCatchThemAll={() => Promise.resolve({ pokemons: [], count: 0 })}
+        getPokemonTypes={() => Promise.resolve([])}
+      />
+    </Router>
   );
 
   getByTestId("spinner");
@@ -19,14 +22,26 @@ it("should display spinner while fetching data", async () => {
 });
 
 it("should display info if there are no pokemons to show", async () => {
-  const { queryByText } = render(<PokemonList gottaCatchThemAll={() => Promise.resolve({ pokemons: [], count: 0 })} />);
+  const { queryByText } = render(
+    <Router>
+      <PokemonList
+        gottaCatchThemAll={() => Promise.resolve({ pokemons: [], count: 0 })}
+        getPokemonTypes={() => Promise.resolve({ types: ["all"] })}
+      />
+    </Router>
+  );
   await waitFor(() => expect(queryByText(/No pokemons/)).toBeTruthy());
 });
 
 it("should display pokemon list items", async () => {
   const { queryAllByTestId } = render(
     <Router>
-      <PokemonList gottaCatchThemAll={() => Promise.resolve({ pokemons: pokemonsData, count: 964 })} />
+      <PokemonList
+        gottaCatchThemAll={() => Promise.resolve({ pokemons: pokemonsData, count: 964 })}
+        getPokemonTypes={() => {
+          return Promise.resolve({ types: ["all"] });
+        }}
+      />
     </Router>
   );
   await waitFor(() => expect(queryAllByTestId("pokemon-list-item")).toHaveLength(20));
