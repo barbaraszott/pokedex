55b8df4472be3b5fe94fb9851935ed4fd43e5099
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-15 11:56:07 +0200

    Create function showEvolutions in PokemonEvolution to show pokemon's evolution chain pictures and make them links to particular pokemons

--- src/components/PokemonEvolution/PokemonEvolution.js
+++ src/components/PokemonEvolution/PokemonEvolution.js
@@ -1,10 +1,34 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 import "./PokemonEvolution.scss";
 import Spinner from "../../tools/Spinner";
+import PokemonPicture from "../PokemonPicture/PokemonPicture";
+
+function showEvolutions(evolutions, levelBackgroundColor) {
+  const evolutionsEntries = Object.entries(evolutions);
+
+  return (
+    <section className="pokemon-evolutions-container">
+      {evolutionsEntries.map(([evolution, evolutionInfo]) => {
+        return (
+          <Link to={`/pokemon/${evolution}`} key={`evolution-${evolution}`} className="pokemon-evolution">
+            <span className="pokemon-evolution__name">{evolution}</span>
+            <figure className="pokemon-evolution__picture">
+              <PokemonPicture picture={evolutionInfo.pictures.front} />
+            </figure>
+            <div className="pokemon-evolution__level" style={{ backgroundColor: levelBackgroundColor }}>
+              {evolutionInfo.level === null ? "?" : evolutionInfo.level}
+            </div>
+          </Link>
+        );
+      })}
+    </section>
+  );
+}
 
 function PokemonEvolution(props) {
-  const { evolutionChainUrl, getEvolutionChain } = props;
+  const { evolutionChainUrl, getEvolutionChain, levelBackgroundColor } = props;
 
   const [isLoading, setLoading] = useState(true);
   const [evolutions, setEvolutions] = useState({});
@@ -16,7 +40,12 @@ function PokemonEvolution(props) {
     });
   }, [evolutionChainUrl, getEvolutionChain]);
 
-  return <>{isLoading ? <Spinner /> : <span>Evolutions fetched</span>}</>;
+  return (
+    <>
+      {isLoading && <Spinner />}
+      {!isLoading && showEvolutions(evolutions, levelBackgroundColor)}
+    </>
+  );
 }
 
 export default React.memo(PokemonEvolution);
