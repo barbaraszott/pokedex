65c59fb9499fa1b37295053a9b7f0776068e1193
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-15 12:13:20 +0200

    Update Pokemons' colors map

--- src/tools/pokemonColor.js
+++ src/tools/pokemonColor.js
@@ -1,10 +1,16 @@
 export default function getPokemonColor(color) {
   const pokemonColor = {
-    green: "#49d0b0",
-    red: "#fc6c6d",
-    blue: "#76befe",
-    yellow: "#ffd76f",
+    blue: "#6AC8FB",
+    green: "#70B44D",
+    red: "#F1474A",
+    yellow: "#F2C045",
+    black: "#0B1811",
+    brown: "#B38A6C",
+    gray: "#737D86",
+    pink: "#FD7D96",
+    purple: "#915A9F",
+    white: "#C6D3C9",
   };
 
-  return pokemonColor[color] || color;
+  return pokemonColor[color];
 }
