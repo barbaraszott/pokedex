286b7b256141d34c2c5c90f1b74b0b20c2d5f201
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-16 17:30:13 +0200

    Updates

---
+++ precache-manifest.eb8b43fd1351701ab42f78d78e746167.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "107db33e9536abf90f57ba480af00d3e",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "db0d1b2ab9ccb2011c63",
+    "url": "/pokedex/static/css/main.5a89b6fb.chunk.css"
+  },
+  {
+    "revision": "1617e7ace2a4c5331a91",
+    "url": "/pokedex/static/js/2.37278f0d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pokedex/static/js/2.37278f0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db0d1b2ab9ccb2011c63",
+    "url": "/pokedex/static/js/main.ada98bc7.chunk.js"
+  },
+  {
+    "revision": "23e4c6896b4e85d076c4",
+    "url": "/pokedex/static/js/runtime-main.3f186e6a.js"
+  },
+  {
+    "revision": "1d367c3a8c06a6c77c883804bc8aad8a",
+    "url": "/pokedex/static/media/pokeball-white.1d367c3a.svg"
+  },
+  {
+    "revision": "86b2cb9086b25bd5616ffa77a21cf929",
+    "url": "/pokedex/static/media/pokeball.86b2cb90.svg"
+  }
+]);
