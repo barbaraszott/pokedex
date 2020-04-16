620132ff724c576e84866384ceccc9bb27974c3d
Barbara Szott <barbara.aleksy@gmail.com>
2020-04-16 15:00:37 +0200

    Update tests for PokemonList - add test checking if error is shown properly

--- src/components/PokemonList/PokemonList.test.js
+++ src/components/PokemonList/PokemonList.test.js
@@ -22,15 +22,15 @@ it("should display spinner while fetching data", async () => {
 });
 
 it("should display info if there are no pokemons to show", async () => {
-  const { queryByText } = render(
+  const { getByTestId } = render(
     <Router>
       <PokemonList
         gottaCatchThemAll={() => Promise.resolve({ pokemons: [], count: 0 })}
-        getPokemonTypes={() => Promise.resolve({ types: ["all"] })}
+        getPokemonTypes={() => Promise.resolve([])}
       />
     </Router>
   );
-  await waitFor(() => expect(queryByText(/No pokemons/)).toBeTruthy());
+  await waitFor(() => expect(getByTestId("no-pokemons")).toBeTruthy());
 });
 
 it("should display pokemon list items", async () => {
@@ -39,10 +39,25 @@ it("should display pokemon list items", async () => {
       <PokemonList
         gottaCatchThemAll={() => Promise.resolve({ pokemons: pokemonsData, count: 964 })}
         getPokemonTypes={() => {
-          return Promise.resolve({ types: ["all"] });
+          return Promise.resolve(["all"]);
         }}
       />
     </Router>
   );
   await waitFor(() => expect(queryAllByTestId("pokemon-list-item")).toHaveLength(20));
+});
+
+it("should set error if error occured", async () => {
+  const { getByTestId, queryByTestId } = render(
+    <Router>
+      <PokemonList
+        gottaCatchThemAll={() => Promise.reject({ pokemons: [], count: 0 })}
+        getPokemonTypes={() => Promise.resolve([])}
+      />
+    </Router>
+  );
+
+  await waitForElementToBeRemoved(() => queryByTestId("spinner"));
+
+  getByTestId("error");
 });
